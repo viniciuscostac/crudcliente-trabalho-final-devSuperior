6aaccdfafacdda760cdd72e29096e97c722b815b
Vinicius do Carmo Costa <vinicius.ccosta@outlook.com>
2022-02-04 16:26:38 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into tb_client (birth_date, children, cpf, income, name) values (now(), 2, '41834675888', 7000.00, 'Vinicius');
+insert into tb_client (birth_date, children, cpf, income, name) values (now(), 1, '41834675458', 5000.00, 'Lorrany');
+insert into tb_client (birth_date, children, cpf, income, name) values (now(), 4, '41836675888', 1000.00, 'Junior');
